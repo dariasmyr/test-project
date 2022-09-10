f651354f175589e800d91800db7358edd1fd5665
dariasmyr <dasha.smyr@gmail.com>
2022-09-10 22:18:16 +0300

    add test project files

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-# Import data from csv file and create a list of dictionaries
-
 import csv
 
-def read_transaction(csv_file_location):
-    csv.register_dialect('empDialect', skipinitialspace=True, strict=True)
-    employee_file = csv.DictReader(open(csv_file_location), dialect = 'empDialect')
-    employee_list = []
-    for data in employee_file:
-        employee_list.append(data)
-    return employee_list
+file = open(
+    r"C:\Users\dasha\work\python-projects\test-project\export-0x715C9eF6e4f3C6c30CE2761371cCa97BB6912345 (1).csv")
 
-employee_list = read_employees('/home/student-00-8e4b5b9b5a9a/data/employees.csv')
-print(employee_list)
+type(file)
+
+csv_reader = csv.reader(file)
+
+with open(
+        r"C:\Users\dasha\work\python-projects\test-project\export-0x715C9eF6e4f3C6c30CE2761371cCa97BB6912345 (1).csv") as file:
+    csv_reader_content = csv.reader(file)
+    next(csv_reader_content)
+    sum = 0
+    for row in csv_reader_content:
+        print(row[3], row[4], row[5], row[8], row[10])
+        sum += float(row[8]) + float(row[10])
+        print(round(sum, 2))
+
+    print("Total sum is: ", round(sum, 2))
 
 
 
-
-
-
-import data as dt
-see = dt.read_csv(r"C:\Users\dasha\work\python-projects\test-project\export-0x715C9eF6e4f3C6c30CE2761371cCa97BB6912345 (1).csv")
-see.to_csv(r"C:\Users\dasha\work\python-projects\test-project\export-0x715C9eF6e4f3C6c30CE2761371cCa97BB6912345 (1).csv")
-print(dt.read_csv(r"C:\Users\dasha\work\python-projects\test-project\export-0x715C9eF6e4f3C6c30CE2761371cCa97BB6912345 (1).csv"))
