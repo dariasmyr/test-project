726c1e3aff197f8b085be5a060a33c4a435705fb
dariasmyr <dasha.smyr@gmail.com>
2022-09-11 02:58:39 +0300

    another commit

--- main.py
+++ main.py
@@ -27,13 +27,6 @@ with open(file) as file:
         endDate = datetime.strptime(endDate_str, '%Y-%m-%d')
         address = row[4]
         if startDate <= date <= endDate and address == '0x715c9ef6e4f3c6c30ce2761371cca97bb6912345':
-            print(row[3], row[4], row[5], row[8], row[10])
             sum += float(row[8]) + float(row[10])
-            print(round(sum, 2))
-            print("Total sum is: ", round(sum, 2))
-        else:
-            print("No transactions found")
-        print("End of the program")
-
-
-
+            print("Total sum is: ", round(sum, 2), "MATIC")
+        else: continue
